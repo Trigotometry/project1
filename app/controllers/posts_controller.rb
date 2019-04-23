1f77dd90659fa116c34a609f38b3ace462041d81
Thomas Hexton <tom.hexton@gmail.com>
2019-04-23 10:11:38 +1000

    user login completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,9 @@ class PostsController < ApplicationController
 
 	def show
 		@topic = Topic.find params[:topic_id]
-		initial_post = @topic.posts.find params[:post_id]
+		initial_post = @topic.posts.find params[:id]
 		@initial_post = initial_post.initial_content
 		@responses = initial_post.responses
-		# raise "hell"
 	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,23 @@
 #
 # Table name: users
 #
-#  id         :bigint(8)        not null, primary key
-#  username   :text
-#  email      :text
-#  image      :text
-#  dob        :date
-#  admin      :boolean
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :bigint(8)        not null, primary key
+#  username        :text
+#  email           :text
+#  image           :text
+#  dob             :date
+#  admin           :boolean
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :text
 #
 
 class User < ApplicationRecord
+	has_secure_password
+
 	has_many :posts
 	has_many :responses
+
+	# Validations -- see the Rails Guide
+	validates :email, :presence => true, :uniqueness => true
 end
